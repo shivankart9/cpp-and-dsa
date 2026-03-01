a55885636c3c5dbe84cd0c8c3894b4f6ec25b452
shivankart9 <shivankart9@gmail.com>
2026-03-01 12:03:07 +0530

    programs that i had learned in lecture  2

---
+++ code3.cpp
@@ -0,0 +1,8 @@
+// to print my firstname and  full name  in different line
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"shivankar\n" << "tiwari";
+    return 0;
+}
---
+++ code6.cpp
@@ -0,0 +1,11 @@
+//program to take input
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter a number: ";
+    cin>>num;
+    cout<<"entred number is:"<<num;
+    return 0;
+}
