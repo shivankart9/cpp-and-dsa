4329230ce1bae52e331859862900730fef8a4e86
shivankart9 <shivankart9@gmail.com>
2026-03-03 17:46:15 +0530

    learnt operators

---
+++ arithmetic-operators.cpp
@@ -0,0 +1,14 @@
+//in this  program there is all types of  arithmetic operators
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=11 , b=5;
+    
+    cout<<"sum = "<<(a+b)<<"\n";
+    cout<<"subtraction = "<<(a-b)<<"\n";
+    cout<<"multiply = "<<(a*b)<<"\n";
+    cout<<"divide = "<<(float)a/b<<"\n";//float is written to get the decimal values
+    cout<<"module = "<<(a%b)<<"\n";
+    return 0;
+}
---
+++ logical-operators.cpp
@@ -0,0 +1,8 @@
+//in this  program there are all the logical operators 
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<< !(3==3)<<"\n";// logical not reverse the answer
+    return 0;
+}
