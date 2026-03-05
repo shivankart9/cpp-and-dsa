8ffbc00a6cebbc9d29e9484d78d4f34aaceee774
shivankart9 <shivankart9@gmail.com>
2026-03-05 21:36:39 +0530

    while loop to print counting

---
+++ while-print-count.cpp
@@ -0,0 +1,17 @@
+// this programs print the numbers/counting using while  loop
+#include<iostream>
+using namespace std;
+int main()
+{
+    int count=1,n;
+    cout<<"enter upto where you want counting :";
+    cin>>n;
+
+    while(count <= n) {
+        cout<<count << " \n";
+        count ++;
+    }
+   
+    
+    return 0;
+}
