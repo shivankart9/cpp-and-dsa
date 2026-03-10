07b529f225ce4dfc68eec4b6b4d512924594f455
shivankart9 <shivankart9@gmail.com>
2026-03-10 20:19:43 +0530

    fix:arranged the files

--- first-program.cpp
+++
@@ -1,8 +0,0 @@
-// first programof my c++
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout<<"my first program in c++";
-    return 0; 
-}
--- name-print.cpp
+++
@@ -1,8 +0,0 @@
-// to print my firstname and  full name  in different line
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout<<"shivankar\n" << "tiwari";
-    return 0;
-}
