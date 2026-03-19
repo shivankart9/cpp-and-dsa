56401574862120a9940166ae5da4278bbfa5c5aa
shivankart9 <shivankart9@gmail.com>
2026-03-19 13:07:32 +0530

    created a basic funtion

---
+++ c++/05_functions/creating-function1.cpp
@@ -0,0 +1,14 @@
+// this program teach us how to make a function and use that
+#include<iostream>
+using namespace std;
+
+// creating a function
+void printhello() {
+    cout<<"hello";
+}
+int main() {
+    // printing the function that we have created
+    printhello();
+
+    return 0;
+}
