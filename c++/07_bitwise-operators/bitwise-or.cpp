e1f1385ebc2fd5a31372b5d3ea9d72a48abadc58
shivankart9 <shivankart9@gmail.com>
2026-03-27 20:59:29 +0530

    this program explain us the concept of bitwise or operator

---
+++ c++/07_bitwise-operators/bitwise-or.cpp
@@ -0,0 +1,10 @@
+// this program explain us the concept of bitwise or operator
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 4 , b = 8 ;
+
+    cout << (a|b) << endl; // 0100 | 1000 = 1100 = 12
+    return 0;
+}
