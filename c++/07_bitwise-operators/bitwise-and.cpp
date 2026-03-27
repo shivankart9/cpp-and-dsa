b75f4efbe05b22f85b0948df7e69571c4e03dd34
shivankart9 <shivankart9@gmail.com>
2026-03-27 20:58:33 +0530

    this program explains us the bit wise and operator

---
+++ c++/07_bitwise-operators/bitwise-and.cpp
@@ -0,0 +1,10 @@
+// this program explains us the bit wise and operator
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 4 , b = 8;
+
+    cout<<(a & b) << endl; // 0100 & 1000 = 0000 = 0
+    return 0;
+}
