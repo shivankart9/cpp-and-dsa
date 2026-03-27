63eb289482bccdf2b918ef28dcbca22fc0686b39
shivankart9 <shivankart9@gmail.com>
2026-03-27 21:01:58 +0530

    this program explains the concept of right shift operator

---
+++ c++/07_bitwise-operators/bitwise-rightshift.cpp
@@ -0,0 +1,10 @@
+// this program explains the concept of right shift operator
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4 ;
+
+    cout << (n >> 1) << endl ; // 100 >> 1 = 10 = 2 // 4 / 2^1 = 2
+    return 0;
+}
