3c5e90c23a3093db5d3757dfd0471006f9951f67
shivankart9 <shivankart9@gmail.com>
2026-03-27 21:01:15 +0530

    this program explains the concept of left shift operator

---
+++ c++/07_bitwise-operators/bitwise-leftshift.cpp
@@ -0,0 +1,10 @@
+// this program explains the concept of left shift operator
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    cout << (n << 1) <<endl; // 100 << 1 = 100_ = 1000 = 8 // 4 x 2^1 = 8
+    return 0;
+}
