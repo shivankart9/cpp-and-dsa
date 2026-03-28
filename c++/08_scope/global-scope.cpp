c5897a20aaa89d5ebc41227f98f821950cc7e95e
shivankart9 <shivankart9@gmail.com>
2026-03-28 19:07:36 +0530

    this program explain the concept of global scope variable

---
+++ c++/08_scope/global-scope.cpp
@@ -0,0 +1,10 @@
+// this program explain the concept of global scope variable
+#include<iostream>
+using namespace std;
+
+int x = 1; // global scope variable 
+
+int main() {
+    cout << x ; // this will not give error
+    return 0;
+}
