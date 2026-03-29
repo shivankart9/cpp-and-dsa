7a38139c3993250d343e05bac78633cb5a9915c7
shivankart9 <shivankart9@gmail.com>
2026-03-29 20:15:10 +0530

    this program creates a array and also store the data in that

---
+++ dsa/02_arrays/creating-storing.cpp
@@ -0,0 +1,11 @@
+// this program creates a array and also store the data in that
+#include<iostream>
+using namespace std;
+
+int main() {
+    int marks[5] = {60,70,90,67,89} ;
+
+    cout << marks[1] ; // prints 70 
+
+    return 0;
+}
