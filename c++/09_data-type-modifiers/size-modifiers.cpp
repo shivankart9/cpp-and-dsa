f375ebfa23f622434ea1f16c8db197cbcfd81596
shivankart9 <shivankart9@gmail.com>
2026-03-29 17:47:46 +0530

    this program explain us how the data type are modified by printing their size before modifier and after

---
+++ c++/09_data-type-modifiers/size-modifiers.cpp
@@ -0,0 +1,13 @@
+// this program explain us how the data type are modified by printing their size before modifier and after 
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    cout << sizeof(int) << endl;
+    cout << sizeof(long int) << endl;
+    cout << sizeof(short int) << endl;
+    cout << sizeof(long long int) << endl;
+    
+    return 0;
+}
