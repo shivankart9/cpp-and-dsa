3c3ed2934739a45f0fba2d409f0e39a98f352abe
shivankart9 <shivankart9@gmail.com>
2026-03-29 17:47:00 +0530

    this program explain us how the sign are modified of data type

---
+++ c++/09_data-type-modifiers/sign-modifiers.cpp
@@ -0,0 +1,14 @@
+// this program explain us how the sign are modified of data type
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    signed int x = -5; //int is default signed
+    unsigned int y = -5; // it will not print -5
+
+    cout << x << endl;
+    cout << y << endl;
+   
+    return 0;
+}
