39cad4b5fc9c8560ab42abcd7aa4ffd05c4bb229
shivankart9 <shivankart9@gmail.com>
2026-03-31 19:17:08 +0530

    this program creates a function for linear search which makes code more efficient

---
+++ dsa/02_arrays/linear-search-function.cpp
@@ -0,0 +1,20 @@
+// this program creates a function for linear search which makes code more efficient
+#include<iostream>
+using namespace std;
+
+int linearsearch(int arr[] , int sz, int target) {
+    for(int i = 0; i < sz; i++) {
+        if(arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {2,3,4,1,8,9,0,10} , sz = 8 ,target = 8;
+
+    cout << linearsearch(arr,sz,target);
+
+    return 0;
+}
