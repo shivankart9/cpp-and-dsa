59370a4ce87b1af1f956122989276fcecbaaa928
shivankart9 <shivankart9@gmail.com>
2026-04-03 12:04:54 +0530

    this program explain us the basic syntax of vector

---
+++ dsa/03_vectors/vector-syntax.cpp
@@ -0,0 +1,14 @@
+// this program explain us the basic syntax of vector
+#include<iostream>
+#include<vector> // header file for vector
+using namespace std;
+
+int main() {
+    vector<int> vec; // creating a vector and writing its typr also
+
+    vector<int> vec1 = {1,2,3} ;// adding data in the vector
+
+    vector<int> vec2(3,0) ;// creating the size of vector and storing 0 there in each place 
+
+    return 0;
+}
