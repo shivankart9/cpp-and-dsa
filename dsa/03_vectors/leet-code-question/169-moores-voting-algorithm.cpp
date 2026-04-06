d83ad0321d1e10a6a2eff686e7cce36a8b4c08f3
shivankart9 <shivankart9@gmail.com>
2026-04-06 12:21:41 +0530

    this program is the answer of leetcode question 169 majority element using moore's voting algorithm

---
+++ dsa/03_vectors/leet-code-question/169-moores-voting-algorithm.cpp
@@ -0,0 +1,28 @@
+// this program is the answer of leetcode question 169 majority element using moore's voting algorithm 
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int majorityelement(vector<int>& nums) {
+    int freq = 0, ans = 0;
+    for(int i = 0; i < nums.size(); i++) {
+        if(freq == 0) {
+            ans = nums[i];
+        }
+        if(ans == nums[i]) {
+            freq++;
+        } else {
+            freq--;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1,2,2,1,1} ;
+
+    cout << "the majority element is : " << majorityelement(nums) << endl;
+
+    return 0;
+}
