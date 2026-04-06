f8d2b9fed141343dfce5ea92483478c61691df9e
shivankart9 <shivankart9@gmail.com>
2026-04-06 12:20:16 +0530

    this is the solution of 136 majority element leet code question this solution is more optised

---
+++ dsa/03_vectors/leet-code-question/169-majority-element-optimised.cpp
@@ -0,0 +1,32 @@
+// this is the solution of 136 majority element leet code question this solution is more optised
+#include<iostream>
+#include<vector>
+#include<algorithm> // used for using sort
+using namespace std;
+
+int majorityelement(vector<int>& nums) {
+    sort(nums.begin(),nums.end()); // for sorting array
+
+    int freq = 1;
+    int ans = nums[0];
+
+    for(int i = 1; i < nums.size(); i++) {
+        if(nums[i] == nums[i-1]) {
+            freq++;
+        } else{
+            freq = 1;
+            ans = nums[i];
+        }
+        if (freq > nums.size()/2) {
+        return ans ;
+     }
+    }
+}
+
+int main() {
+    vector<int> nums = {1,2,2,1,1};
+    
+    cout << "the majority element is :  " << majorityelement(nums) << endl;
+
+    return 0;
+}
