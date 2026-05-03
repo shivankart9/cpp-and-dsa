d0bf047ea83f0eafac106da9eb91023a8e878da3
shivankar tiwari <shivankart9@gmail.com>
2026-05-03 20:53:41 +0530

    unordered map

---
+++ c++/11_standard-template-library/non-sequence-container/map/unorderedmap.cpp
@@ -0,0 +1,12 @@
+// unordered map. in this program we will create unordered map 
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int main() {
+    unordered_map<string,int> m;
+
+    return 0;
+}
+
+// other things we will learn in hashing
