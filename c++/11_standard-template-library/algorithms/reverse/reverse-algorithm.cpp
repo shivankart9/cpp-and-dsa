4bd9b5da70f680b57890a52092ba2257c7ed6e83
shivankar tiwari <shivankart9@gmail.com>
2026-05-04 12:00:00 +0530

    reverse algorithm

---
+++ c++/11_standard-template-library/algorithms/reverse/reverse-algorithm.cpp
@@ -0,0 +1,17 @@
+// this program explai us how reverse lgorithm works
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> vec = {1,2,3,4,5};
+
+    reverse(vec.begin(),vec.end());
+
+    for(int val : vec) {
+        cout << val << " " ;
+    }
+
+    return 0;
+}
