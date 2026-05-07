6aa4b687bd0a61db9610799dcc8eb722609bccaa
shivankar tiwari <shivankart9@gmail.com>
2026-05-07 10:25:54 +0530

    creating string

---
+++ dsa/04_strings/strings/creating-string.cpp
@@ -0,0 +1,12 @@
+// crating string
+#include<iostream>
+#include<string> // optional
+using namespace std;
+
+int main() {
+    string str = "shivankar tiwari";
+
+    cout << str << endl;
+
+    return 0;
+}
