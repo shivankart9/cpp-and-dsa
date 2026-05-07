528e967b11465e4baa307d5b473a5f0d15034f32
shivankar tiwari <shivankart9@gmail.com>
2026-05-07 10:22:21 +0530

    creating cstring

---
+++ dsa/04_strings/cstrings/creating-cstring.cpp
@@ -0,0 +1,11 @@
+// in this program we learn how to create string
+#include<iostream>
+using namespace std;
+
+int main() {
+    char str[] = {'a','b','c','\0'}; // add '\0' at last it works as boundry marker
+
+    cout << str << endl;
+
+    return 0;
+}
