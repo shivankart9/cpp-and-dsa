0555540853c148adc2c9f83da8c5abe8ec9e414c
shivankar tiwari <shivankart9@gmail.com>
2026-05-07 10:26:31 +0530

    concatination

---
+++ dsa/04_strings/strings/concatination.cpp
@@ -0,0 +1,14 @@
+// conactination means to add two strings 
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string str1 = "shivankar";
+    string str2 = "tiwari";
+    string str3 = str1+str2; // concatincation
+
+    cout << str3 << endl;
+
+    return 0;
+}
